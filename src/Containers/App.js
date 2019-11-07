95a28d2746031ee2054638c1ccdf5d1d05a98096
Changretta <johnylaf94@gmail.com>
2019-11-07 09:59:39 +0100

    redux middleware and async actions

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,17 +1,30 @@
 import React, {Component} from 'react'
+import  { connect } from 'react-redux'
+
 //import ReactDOM from 'react-dom'
 import SearchBox from '../Components/SearchBox'
 import CardList from '../Components/CardList'
 import './App.css';
 import Scroll from '../Components/scroll';
 import robots from '../robots'
+import {setSearchField} from '../actions'
+
+const mapStateToProps =  state => {
+    return {
+        searchField: state.searchField
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
  class App extends Component {
      constructor(){
          super()
          this.state = {
-            robots:[],
-            searchField: "",
+            robots:[]
          }
      }
      componentDidMount(){
@@ -23,13 +36,14 @@ import robots from '../robots'
         })
        }
 
-     onSearchChange = (e) => {
-         this.setState({searchField : e.target.value})
+//      onSearchChange = (e) => {
+//          this.setState({searchField : e.target.value})
         
-//        console.log(filteredRobots);
-     } 
+// //        console.log(filteredRobots);
+//      } 
      render () {
-        const { robots, searchField} = this.state;
+        const { robots} = this.state;
+        const { searchField, onSearchChange } = this.props
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
@@ -39,7 +53,7 @@ import robots from '../robots'
          return(
         <div className='tc'>
             <h1 className='f2'> Robo Friends </h1>
-            <SearchBox searchChange={this.onSearchChange}/>
+            <SearchBox searchChange={onSearchChange}/>
             <Scroll>           
                  <CardList robots={filteredRobots}/>
             </Scroll>
@@ -50,4 +64,4 @@ import robots from '../robots'
     
 }
 
-export default App
+export default connect(mapStateToProps , mapDispatchToProps)(App);
